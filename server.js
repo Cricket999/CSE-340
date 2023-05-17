3cbf54c90d8d47a12d142f3c7207225bba291d3a
Cricket999 <98436415+Cricket999@users.noreply.github.com>
2023-05-17 00:16:30 -0600

    Changes through U3 Application Service Assignment

--- server.js
+++ server.js
@@ -8,11 +8,24 @@
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
+const expressLayouts = require("express-ejs-layouts")
+const baseController = require("./controllers/baseController")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
+//Index route
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", require("./routes/inventoryRoute"))
 
 /* ***********************
  * Local Server Information
